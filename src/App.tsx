bac488e303fed2c9b8d21881b86fee92d6507642
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-05 06:05:29 +0100

    Set up project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">yo</div>;
+}
+
+export default App;
+// form with
+// description, amount, category (Groceries, Utilities, Entertainment), button
+// table
+// dropdown All categories, description, amount, category
+// table
+// description, amount, category, delete button
+// list all the item in selected categories
+// list total at the end
