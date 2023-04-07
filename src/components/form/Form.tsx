923a94c39680f4f41de7e52e03326a94ee2af961
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-07 14:50:29 +0100

    Add form validation with zod

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,33 +1,72 @@
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FieldValues, useForm } from "react-hook-form";
+
+const schema = z.object({
+  description: z.string().min(3),
+  amount: z.number().min(1),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
-          <label htmlFor="" className="form-label">
+          <label htmlFor="description" className="form-label">
             Description
           </label>
-          <input type="text" className="form-control" />
+          <input
+            {...register("description")}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+          {errors.description && (
+            <p className="text-danger">{errors.description.message}</p>
+          )}
         </div>
         <div className="mb-3">
-          <label htmlFor="" className="form-label">
+          <label htmlFor="amount" className="form-label">
             Amount
           </label>
-          <input type="text" className="form-control" />
+          <input
+            {...register("amount", { valueAsNumber: true })}
+            id="amount"
+            type="text"
+            className="form-control"
+          />
+          {errors.amount && (
+            <p className="text-danger">{errors.amount.message}</p>
+          )}
         </div>
         <div className="mb-3">
-          <label htmlFor="" className="form-label">
+          <label htmlFor="category" className="form-label">
             Category
           </label>
-          <select className="form-select" aria-label="Default select example">
+          <select
+            id="category"
+            className="form-select"
+            aria-label="Default select example"
+          >
             <option selected>Category</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
           </select>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button disabled={!isValid} type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
